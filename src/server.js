2140f14c78889d9b04c618406aa0a94616c4a8ee
Oleh Butenko <butenkoleh@gmail.com>
2025-11-19 19:27:26 +0200

    add auth logic

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -19,8 +21,10 @@ app.use(
   }),
 );
 app.use(cors());
+app.use(cookieParser());
 
 app.use(notesRoutes);
+app.use(authRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
